a80060c0b39265cc68d13fdebc8318a8d31003ae
Nam Luu <namluu253@gmail.com>
2022-08-18 10:08:40 +0700

    update oh my zsh install script

--- setup.sh
+++ setup.sh
@@ -5,11 +5,6 @@ cd ~
 # homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# zsh2000 theme
-git clone https://github.com/maverick9000/zsh2000.git
-mv ~/zsh2000/zsh2000.zsh-theme ~/.oh-my-zsh/themes
-rm -rf zsh2000
-
 # poweline font
 git clone https://github.com/powerline/fonts.git --depth=1
 cd fonts
@@ -18,13 +13,16 @@ cd ..
 rm -rf fonts
 
 # iterm material theme
-curl -o material-design-colors.itermcolors https://raw.githubusercontent.com/MartinSeeler/iterm2-material-design/master/material-design-colors.itermcolors
+wget https://raw.githubusercontent.com/MartinSeeler/iterm2-material-design/master/material-design-colors.itermcolors
 
 # install Homebrew tap
 brew tap Homebrew/bundle
 
 # moving config file
-mv ~/mac-setup/*(DN) ~/
+cd ~
+shopt -s dotglob
+mv ~/mac-setup/* ~/
+# mv ~/mac-setup/*(DN) ~/
 rm -rf .git
 
 # dump formulae/cask 
@@ -37,6 +35,9 @@ git config --global core.editor nano
 
 # nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install --lts
 nvm alias default lts/*
 
@@ -47,11 +48,20 @@ git clone https://github.com/Jean-Tinland/simple-bar $HOME/Library/Application\ 
 brew services start yabai
 brew services start skhd
 
+# oh-my-zsh
+#sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc
+sh oh-my-zsh.sh --keep-zshrc
+compaudit | xargs chmod g-w,o-w
+
 # zsh syntax highlight
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # zsh auto suggestion
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-# oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# zsh2000 theme
+git clone https://github.com/maverick9000/zsh2000.git
+mv ~/zsh2000/zsh2000.zsh-theme ~/.oh-my-zsh/themes
+rm -rf zsh2000
+
+exec zsh -l
