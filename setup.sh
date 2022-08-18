8327df35dfc569222f1f101e95ca70cc368afa62
Nam Luu <namluu253@gmail.com>
2022-08-18 10:41:50 +0700

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,39 +1,43 @@
 #!/bin/sh
 
+# color setup
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+rm -rf .git
 cd ~
 
-# homebrew
+printf "${RED}install homebrew${NC}\n"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# poweline font
+printf "${RED}install poweline font${NC}\n"
 git clone https://github.com/powerline/fonts.git --depth=1
 cd fonts
 ./install.sh
 cd ..
 rm -rf fonts
 
-# iterm material theme
+printf "${RED}install iterm material theme${NC}\n"
 wget https://raw.githubusercontent.com/MartinSeeler/iterm2-material-design/master/material-design-colors.itermcolors
 
-# install Homebrew tap
+
+printf "${RED}install install Homebrew tap${NC}\n"
 brew tap Homebrew/bundle
 
-# moving config file
+printf "${RED}moving config file${NC}\n"
 cd ~
 shopt -s dotglob
 mv ~/mac-setup/* ~/
-# mv ~/mac-setup/*(DN) ~/
-rm -rf .git
-
-# dump formulae/cask 
+ 
+printf "${RED}dumping formulae/cask${NC}\n"
 brew bundle
 
-# github config
+printf "${RED}configuring github${NC}\n"
 git config --global user.email namluu253@gmail.com
 git config --global user.name "Nam Luu"
 git config --global core.editor nano
 
-# nvm
+printf "${RED}setup NVM${NC}\n"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
@@ -41,25 +45,25 @@ export NVM_DIR="$HOME/.nvm"
 nvm install --lts
 nvm alias default lts/*
 
-# menubar
+printf "${RED}install menubar${NC}\n"
 git clone https://github.com/Jean-Tinland/simple-bar $HOME/Library/Application\ Support/Übersicht/widgets/simple-bar
 
-# autorun yabai and skhd
+printf "${RED}make yabai/skhd autostart${NC}\n"
 brew services start yabai
 brew services start skhd
 
-# oh-my-zsh
+printf "${RED}install oh-my-zsh${NC}\n"
 #sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc
 sh oh-my-zsh.sh --keep-zshrc
 compaudit | xargs chmod g-w,o-w
 
-# zsh syntax highlight
+printf "${RED}install zsh syntax highlighting${NC}\n"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-# zsh auto suggestion
+printf "${RED}install zsh auto suggestion${NC}\n"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-# zsh2000 theme
+printf "${RED}install zsh2000 theme${NC}\n"
 git clone https://github.com/maverick9000/zsh2000.git
 mv ~/zsh2000/zsh2000.zsh-theme ~/.oh-my-zsh/themes
 rm -rf zsh2000
