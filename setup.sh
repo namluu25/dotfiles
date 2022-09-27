6d50ba5a8c8bd9395f0e755db8c7b5ef443d7ceb
Nathan <namluu253@gmail.com>
2022-09-27 22:59:32 +0700

    update platform architecture check on setup script

--- setup.sh
+++ setup.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
+sudo xcodebuild -license accept
 
 # color setup
 RED='\033[0;31m'
 NC='\033[0m' # No Color
+
+MACHINE_TYPE=`uname -m`
+shasum=$(shasum -a 256 $(which yabai))
 
 rm -rf .git
 cd ~
 
 printf "${RED}install homebrew${NC}\n"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+if [ ${MACHINE_TYPE} == 'arm64' ]; then
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/[your-user-name]/.zprofile
+	eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
 
 printf "${RED}install poweline font${NC}\n"
 git clone https://github.com/powerline/fonts.git --depth=1
@@ -37,6 +46,9 @@ git config --global core.editor nano
 
 printf "${RED}setup NVM${NC}\n"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install --lts
 nvm alias default lts/*
 
@@ -65,6 +77,5 @@ mv ~/zsh2000/zsh2000.zsh-theme ~/.oh-my-zsh/themes
 rm -rf zsh2000
 
 echo -e "${RED}Run these later${NC}\nbrew install cloudflare-warp\nbrew install karabiner-elements\nbrew install teamviewer\n"
-shasum=$(shasum -a 256 $(which yabai))
 echo -e "Run ${RED}sudo visudo -f /private/etc/sudoers.d/yabai${NC} and paste following line bellow (press i for ${RED}INSERT${NC}, ${RED}ESC+':wq'${NC} for save and quit \nnamluu ALL = (root) NOPASSWD: $shasum  --load-sa"
 exec zsh -l
